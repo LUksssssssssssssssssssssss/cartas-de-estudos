caf9a04c4e289afa9455aa40cbc61742fc79c3b6
LUksssssssssssssssssssssss <mgarridonascimento@gmail.com>
2024-11-07 15:00:32 -0300

    Add files via upload

---
+++ assets/perguntas.js
@@ -0,0 +1,71 @@
+criaCartao(
+   'Programação',
+   'O que é CSS?',
+   'O CSS é uma linguagem de programação que serve para dar estilo a páginas web'
+)
+
+ criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+ )
+
+ criaCartao(
+    'Programação',
+    'O que é HTML?',
+    'O HTML é uma linguagem de programação que define a estrutura e o significado do conteúdo de uma página web'
+ )
+
+ criaCartao(
+   'Lingua Estrangeira',
+   'Como se diz oi em inglês?',
+   'Oi em inglês se diz hi'
+)
+
+criaCartao(
+   'Lingua Estrangeira',
+   'Como se diz doente em irlandês?',
+   'Doente em irlanês é tinn'
+)
+
+criaCartao(
+   'Lingua Estrangeira',
+   'Como se diz comida em Inglês?',
+   'Comida em inglês é food'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual a capital do Panamá?',
+   'A capital do Panamá é Cidade do Panamá'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual a capital do Canadá?',
+   'A capital do Canadá é Ottawa'
+)
+
+criaCartao(
+   'Geografia',
+   'Qual a capital da Austrália?',
+   'A capital da Austrália é Canberra '
+)
+
+criaCartao(
+   'Matemática',
+   'Qual a raiz quadrada de 9',
+   'A raiz quadrada de 9 é 3'
+)
+
+criaCartao(
+   'Matemática',
+   'Quanto é 350x456?',
+   '159,600'
+)
+
+criaCartao(
+   'Matemática',
+   'O que é um número primo?',
+   'Um número primo é um número natural que é maior que 1 e possui apenas dois divisores: 1 e ele próprio. Por exemplo, 2, 3, 5, 7, 11 são números primos.'
+)
